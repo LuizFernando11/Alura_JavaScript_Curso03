a7d0f731d80181d82de972802fa37027cad3168d
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2025-03-27 23:26:28 -0300

    Exericio 2.5
    Exercicio

--- Exercicios/Modulo01/app/main.js
+++ Exercicios/Modulo01/app/main.js
@@ -10,8 +10,22 @@ async function BuscarLivrosDaAPI() {
     livros = await resultado.json();
     console.table(livros);
 
-    ImprimirLivros(livros)
+    let LivrosComDescontos = desconto(livros);
+
+    ImprimirLivros(LivrosComDescontos)
     
+}
+
+function desconto(livros){
+
+    const desconto = 0.5;
+
+    valorFInal = livros.map(livro => {
+        return {...livro, preco: livro.preco - (livro.preco * desconto)};
+    })
+
+    return valorFInal;
+
 }
 
 function ImprimirLivros(livros){
@@ -23,7 +37,7 @@ function ImprimirLivros(livros){
             <img id="imagem" src="${livro.imagem}" alt="${livro.alt}">
             <h1 id="titulo">${livro.titulo}</h1>
             <p id="autor">${livro.autor}</p>
-            <p id="preco">R$${livro.preco}</p>
+            <p id="preco">R$${livro.preco.toFixed(2)}</p>
             <div id="tags">
                 <p id="tag">${livro.categoria}</p>
             </div>
